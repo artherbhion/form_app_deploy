db903bec0c1cfa0bbffe9b9cfaea1b05b8b04001
artherbhion <147584933+artherbhion@users.noreply.github.com>
2024-10-08 11:20:07 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+export default function App() {
+  function Submit(e) {  
+    
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+
+    alert("Form submitted successfully!");
+    fetch("https://script.google.com/macros/s/AKfycbzBHewgidZ5kM1QM1To8WCFCAWV-n3ScLfEg2ZjKgFFepqZ0AOpNyjvHZJk3PzF56J9WQ/exec", {
+      method: "POST",
+      body: formDatab,      
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);  
+      alert("Form submitted successfully!");
+    })
+    .catch((error) => {
+      console.log(error);  
+      alert("Form submitted successfully!");
+    });
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+      <h1>USER DATA FORM</h1>
+      <div>
+        <form className="form" onSubmit={(e) => Submit(e)}>
+          <input placeholder="Enter Name" name="Name" type="text" />
+          <input placeholder="Enter Email" name="Email" type="email" />
+          <input placeholder="Enter Adress" name="Message" type="text" />
+          <input placeholder="Enter phone Number" name="Phone" type="text" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
